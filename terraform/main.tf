9c4e350d72a70d6c7e9ccb608752c81f302a1809
Esckay <esckays@gmail.com>
2023-07-16 17:31:18 +0200

    initial eks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,4 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-
-  backend "remote" {
-    organization = "Esckays"
-    workspaces {
-      name = "Terraform"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.8"
-    }
-  }
-}
-
-provider "aws" {
-  region  = var.region
-  profile = var.profile
-}
+#
